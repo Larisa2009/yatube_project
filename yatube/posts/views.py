e54f864fc53d4c89472abca89e5625b44d1cf6b7
Лариса Желбанова <zhelbanovalarisa@mail.ru>
2022-12-27 17:32:22 +0800

    add static

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render
 
 # Create your views here.
 # Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    template = 'posts/index.html'
+    context = {
+        # В словарь можно передать переменную
+        'message': 'Это главная страница проекта Yatube',
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        
+    }
+    return render(request, template, context)
+    
 
 
 # Страница со списком мороженого
 def group_posts(request, slug):
-    return HttpResponse(f'Посты, отфильтрованные по группам {slug}')
+    template = 'posts/group_list.html'
+    context = {
+        # В словарь можно передать переменную
+        'message': 'Здесь будет информация о группах проекта Yatube',
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        
+    }
+    return render(request, template, context)
+    
